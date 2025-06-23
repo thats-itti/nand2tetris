a8025c3bb29b425f431098455d65a6d3c86e34b1
spl <splsnitv@gmail.com>
2025-06-23 11:48:40 +0545

    after week 4

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -6,5 +6,81 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
-
 //// Replace this comment with your code.
+
+@R1
+D = M
+@n
+M = D
+
+@0
+D = 0
+@R2
+M = D
+
+@R0
+D = M
+@zero
+D;JEQ
+@R1
+D = M
+@zero
+D;JEQ
+
+
+@R0
+D = M -1
+@oneR1
+D;JEQ
+
+@R1
+D = M -1
+@oneR2
+D;JEQ
+
+@loop
+0;JMP
+
+
+(oneR1)
+@R1
+D = M
+@R2
+M = D
+@exit
+0;JMP
+
+
+(zero)
+@0
+D = A
+@R2
+M = 0
+@exit
+0;JMP
+
+
+(oneR2)
+@R0
+D = M
+@R2
+M = D
+@exit
+0;JMP
+
+
+
+
+
+(loop)
+@R0
+D = M
+@R2
+M = M + D
+@n
+M = M -1
+D = M
+@loop
+D;JNE
+(exit)
+
